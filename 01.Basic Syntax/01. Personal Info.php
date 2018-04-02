6338e658a34ed5878945fa1e46520677cbad71e1
aldpeev <aldpeev@gmail.com>
2018-04-02 13:50:03 +0300

    PHP basic syntax added

---
+++ 01.Basic Syntax/01. Personal Info.php
@@ -0,0 +1,9 @@
+<?php
+
+function test($firstName, $lastName, $age)
+{
+    echo "My name is " . "{$firstName} " . "{$lastName} " . "and I am " . "{$age}" . " years old." . "<br>";
+}
+
+test("Pesho", "Peshev", "55");
+
